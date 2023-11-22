82c6fc4398ac2496f24960790ae9ca9725cdb105
senagulkorkmaz <senagulkorkmazz@gmail.com>
2023-11-22 11:18:20 +0300

    Link Güncellemeleri Yapıldı.

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const express = require('express');
 const app = express();
 
 
+// Template Engine
+app.set("view engine", "ejs");
+
+
+// Middlewares
+app.use(express.static("public"));
+
+
+// Routes
 app.get('/', (req, res) => {
-    res.status(200).send('INDEX SAYFASI')
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+    });
 });
 
 
